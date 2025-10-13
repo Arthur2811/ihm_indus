a853b9379ff2be9654a1a008fe4e19be64a593f3
Arthur2811 <arthur.cadran@gmail.com>
2025-10-13 11:55:40 +0200

    V26

--- web_indus/run.py
+++ web_indus/run.py
@@ -2,7 +2,7 @@ import os
 import sys
 from pathlib import Path
 
-# Ajouter le répertoire racine au PYTHONPATH
+# Ajouter le répertoire racine au PYTHONPATH.
 root_dir = Path(__file__).parent
 sys.path.insert(0, str(root_dir))
 
